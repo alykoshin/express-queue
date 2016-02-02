5d52b430bc397f13ba46d4ea1b058d39f998a24b
Alexander <alykoshin@gmail.com>
2016-02-03 00:43:37 +0300

    minor changes

--- demo/app.js
+++ demo/app.js
@@ -1,6 +1,7 @@
 'use strict';
 
 
+var debug   = require('debug')('app');
 var express = require('express');
 var http    = require('http');
 
@@ -16,6 +17,8 @@ var app = express();
 app.use(queue({ activeLimit: 2 }));
 
 
+var RESPONSE_DELAY = 1000; // Milliseconds
+
 app.get('/test1', function (req, res) {
   console.log('get(test1): 1');
 
@@ -27,7 +30,7 @@ app.get('/test1', function (req, res) {
       .status(200)
       .send(result);
     console.log('get(test1): 3');
-  }, 1000);
+  }, RESPONSE_DELAY);
 
 });
 

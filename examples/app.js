625cea1a6e065f45a72f6585983827d4c7f3ea42
endrcnn <ender@cbot.ai>
2022-09-12 23:46:49 +0300

    added field configuration to make multiple queue

--- examples/app.js
+++ examples/app.js
@@ -13,7 +13,7 @@ const httpPort = 8080;
 const app = express();
 
 // Using queue middleware
-const queueMw = expressQueue({ activeLimit: 2, queuedLimit: 6 });
+const queueMw = expressQueue({ activeLimit: 2, queuedLimit: 2, field:"userId" });
 app.use(queueMw);
 // May be also:
 // app.use(queue({ activeLimit: 2, queuedLimit: -1 }));
